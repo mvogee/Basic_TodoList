39674df2695ade26e558c023a47c2c9c58cfdbea
Matthew Vogee <mvogee@yahoo.com>
2021-04-26 22:14:57 -0700

    updating checkbox status in mongoDB working. checked boxes are breaking post requests

--- app.js
+++ app.js
@@ -3,9 +3,21 @@ const app = express();
 const bodyParser = require("body-parser");
 const getToday = require(__dirname + "/getDate.js");
 
+
 const mongoose = require("mongoose");
-
-
+const url = "mongodb://localhost:27017/todoList";
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+const todoSchema = new mongoose.Schema({
+    todoListItem: {
+        type: String,
+        required: [true, "no list item name"]
+    },
+    todoDone: {
+        type: Boolean,
+        required: [true, "is it finished or not"]
+    }
+});
+const Todo = mongoose.model("Todo", todoSchema);
 //TODO: Add new lists (create a name for the list and button to create it)
 //TODO: routing for dinamicly created pages.
 //TODO: add Delete button next to items
@@ -20,79 +32,103 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-function dbAdd(dataObj, collection) {
-    const url = "mongodb://localhost:27017/todoList";
-    const options = {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
+function dbAdd(dataObj) {
+    console.log("Adding object to mongodb");
+    mongoose.connect(url, options, () => {console.log("server connection made")});
+    const db = mongoose.connection;
+    db.on("error", console.error.bind(console, "connection error"));
+    console.log("test1");
+
+    const todo = new Todo({
+        todoListItem: dataObj.name,
+        todoDone: dataObj.isDone
+    });
+    todo.save((err) => {
+        console.log("Item saved to database");
+        if (err) {
+            console.log(err)    
+        }
+        mongoose.disconnect();
+    });
+}
+
+function renderPage(pageName, res) {
+    mongoose.connect(url, options, () => {console.log("mongoose connection made")});
+    const db = mongoose.connection;
+
+    db.on("error", console.error.bind(console, "connection error"));
+    Todo.find({}, (err, result) => {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            ejsObj = {
+                listTitle: pageName,
+                listItems: result
+            }
+            res.render("list", ejsObj);
+        }
+        mongoose.disconnect();
+    });
+}
+
+function updateCheckboxState(req) {
     mongoose.connect(url, options);
     const db = mongoose.connection;
     db.on("error", console.error.bind(console, "connection error"));
-    db.once("open", () => {
-        console.log("db connection open");
-        const todoSchema = new mongoose.Schema({
-            todoListItem: {
-                type: String,
-                required: [true, "no list item name"]
-            },
-            todoDone: {
-                type: Boolean,
-                required: [true, "is it finished or not"]
-            }
-        });
-        todoSchema.methods.getisFinished = function() {
-            return this.todoFinished;
-        };
-        todoSchema.methods.getListItem = function() {
-            return this.todoListItem;
+    console.log(req.body);
+    Todo.findOne({_id: req.body.checkbox}, (err, result) => {
+        console.log(result);
+        if (err) {
+            console.log(err);
         }
-        const Todo = mongoose.model(collection, todoSchema);
-
-        const todo = new Todo({todoListItem: dataObj.name , todoDone: dataObj.isDone });
-        todo.save((err) => {console.log(err)});
+        else if (!result) {
+            console.log("result was null\nReturning before failure\n"); // currently always failing if the box is checked
+            return ;
+        }
+        console.log(result.todoDone); // giving null only when box is checked
+        result.todoDone = (result.todoDone === true ? false : true); 
+        result.save(() => {mongoose.disconnect();});
     });
 }
 
 app.get("/", (req, res) => {
+    
+    renderPage(getToday.getToday(), res);
 
-    let ejsObj = {
-        listTitle: getToday.getToday(),
-        listItems: listItems
-    };
-    res.render("list", ejsObj);
-})
-
-app.get("/work", (req, res) => {
-    let ejsObjs = {
-        listTitle: "Work",
-        listItems: workListItems
-    }
-    res.render("list", ejsObjs);
-})
+});
 
 app.get("/about", (req, res) => {
     res.render("about");
-})
+});
 
 
 app.post("/", (req, res) => {
-    console.log(req.body);
+    console.log("post request received");
+    //console.log(req.body);
+    workListItems.push(req.body.newItem); // hook up to the database here
+    const todoObj = {
+        name: req.body.newItem,
+        isDone: false
+    }
     if(req.body.list === "Work") { // make dinamic pages. This will change to dinamic routing from ejs
-        workListItems.push(req.body.newItem); // hook up to the database here
-        const todoObj = {
-            name: req.body.newItem,
-            isDone: false
-        };
-        dbAdd(todoObj, "Todo");
+        
+        dbAdd(todoObj);
         res.redirect("/work");
     }
     else {
-        dbAdd(todoObj, "Todo");
-        listItems.push(req.body.newItem); // here hook it up to databse
+        dbAdd(todoObj);
+        console.log("past dbAdd");
         res.redirect("/");
     }
-    
+
+});
+
+app.post("/itemChecked", (req, res) => {
+   console.log(req.body);
+   updateCheckboxState(req);
+   // update the database at req.body.checkbox
+   res.redirect("/"); 
 });
 
 app.listen(port, () => {
